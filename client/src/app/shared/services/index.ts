32550a9d281cdfb64a38aa8e49903630980ca0a6
Michael Luo <michael.luo0801@gmail.com>
2020-02-05 20:56:12 +1100

    fix product detail page

--- client/src/app/shared/services/index.ts
+++ client/src/app/shared/services/index.ts
@@ -1,7 +1,11 @@
 import { Provider } from '@angular/core';
 
 import { ProductService, HttpProductService } from './product.service';
+import { BidService } from './bid.service';
 
 export { Product, ProductSearchParams, ProductService } from './product.service';
 
-export const SHARED_SERVICES: Provider[] = [{ provide: ProductService, useClass: HttpProductService }];
+export const SHARED_SERVICES: Provider[] = [
+  { provide: ProductService, useClass: HttpProductService },
+  { provide: BidService, useClass: BidService }
+];
